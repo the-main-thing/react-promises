51b5d2f19c016e144f4cc159ee1c5ab52e8b93d8
Pavel Shevtsov <mail@pavelshevtsov.com>
2021-11-27 18:35:30 +0300

    useKitchen

--- src/Restaurant.jsx
+++ src/Restaurant.jsx
@@ -4,42 +4,41 @@ import MenuButtons from './MenuButtons'
 import Dish from './Dish'
 import cookAnOrder from './cookAnOrder'
 
-function Kitchen({ order }) {
+function useKitchen(order) {
   const [dish, setDish] = useState('')
   const [isCooking, setIsCooking] = useState(false)
   useEffect(() => {
     if (order) {
-      setIsCooking(true)
       let stillAwaiting = true
+      setIsCooking(true)
       async function cook() {
-        console.log('Начинаем готовить', order)
         const dish = await cookAnOrder(order)
-        console.log(order, 'готовы.')
         if (stillAwaiting) {
-          console.log('Сервируем', order)
           setIsCooking(false)
           setDish(dish)
         }
       }
       cook()
-      return function cleanUp() {
+      return () => {
         stillAwaiting = false
       }
     }
   }, [order])
-
-  return <Dish dish={dish} isCooking={isCooking} />
+  return {
+    dish,
+    isCooking,
+  }
 }
 
 export default function Restaurant() {
-  // блюдо в тарелке
-  const [dish, setDish] = useState('')
+  const [order, setOrder] = useState('')
+  const { dish, isCooking } = useKitchen(order)
 
   return (
     <div>
       <h2>Ресторан</h2>
-      <MenuButtons onDishSelect={setDish} />
-      <Kitchen order={dish} />
+      <MenuButtons onDishSelect={setOrder} />
+      <Dish dish={dish} isCooking={isCooking} />
     </div>
   )
 }
